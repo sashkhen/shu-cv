d44297e5df0576b5b24d0e299cf27a00dcc8e21f
Sasha Diachenko <sashkhen056@gmail.com>
2024-11-08 17:49:01 +0100

    Add homepage and deploy scripts for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "",
   plugins: [react()],
   css: {
     modules: {
@@ -11,5 +12,8 @@ export default defineConfig({
         return `${filename.split("/").reverse()[0].split(".")[0]}__${name}`;
       },
     },
+  },
+  build: {
+    outDir: "./build",
   },
 });

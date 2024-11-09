7f93198362bc0811beca4ce3b222a89c23038ee4
Sasha Diachenko <sashkhen056@gmail.com>
2024-11-09 13:12:17 +0100

    add css modules prod

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "",
-  plugins: [react()],
-  css: {
-    modules: {
-      generateScopedName: "[folder]__[local]__[hash:base64:5]",
+export default defineConfig(({ command }) => {
+  const isProduction = command === "build";
+
+  return {
+    base: "",
+    plugins: [react()],
+    css: {
+      modules: {
+        generateScopedName: isProduction
+          ? "[hash:base64:5]"
+          : "[folder]__[local]__[hash:base64:5]",
+      },
     },
-  },
-  build: {
-    outDir: "./build",
-  },
+    build: {
+      outDir: "./build",
+    },
+  };
 });
